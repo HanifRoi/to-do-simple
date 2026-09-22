6630da965e07909f095da8863f8321961a3f0620
unknown <roi.hanifroihan@gmail.com>
2026-09-22 20:54:00 +0700

    Membuat Logic Login

--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -8,10 +8,33 @@ use Tests\TestCase;
 
 class UserControllerTest extends TestCase
 {
-    public function testLogin()
+    public function testLoginPage()
     {
         $this->get('/login')->assertSeeText("Login");
     }
+
+    public function testLoginSuccess()
+    {
+        $this->post('/login', [
+            'user'=> 'roihan',
+            'password'=>'rahasia'
+        ])->assertRedirect('/')
+            ->assertSessionHas("user", "roihan");
+    }
+
+    public function testLoginError()
+    {
+        $this->post('/login',[])->assertSeeText('User Atau Password harus diisi');
+    }
+
+    public function testLoginWrong()
+    {
+        $this->post('/login', [
+            'user' => 'roihan',
+            'password'=> 'bonbon'
+        ])->assertSeeText('User Atau Password salah');
+    }
+
 
 
 }
